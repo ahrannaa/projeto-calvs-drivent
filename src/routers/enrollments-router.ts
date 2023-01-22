fbd42807f8b8164acc9d61a3d05729f2e4038276
Ahranna Araujo <ahrannaaraujo@ahrannas-air.lan>
2023-01-22 11:18:12 +0000

    implementation of the enrollments route

--- src/routers/enrollments-router.ts
+++ src/routers/enrollments-router.ts
@@ -9,6 +9,7 @@ enrollmentsRouter
   .get("/cep", getAddressFromCEP)
   .all("/*", authenticateToken)
   .get("/", getEnrollmentByUser)
-  .post("/", validateBody(createEnrollmentSchema), postCreateOrUpdateEnrollment);
+  .get("/enrollments/cep", getAddressFromCEP)
+  .post("/", validateBody(createEnrollmentSchema), postCreateOrUpdateEnrollment)
 
 export { enrollmentsRouter };
